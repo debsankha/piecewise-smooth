3ef3b0c530b882267a75a6fec432597672f9414e
Debsankha Manik <nofrills@xfce4-desktop.iiserkol.ac.in>
2013-04-04 18:30:27 +0530

    Added probe_vs_n

--- pws/impactmap.py
+++ pws/impactmap.py
@@ -3,6 +3,10 @@
 #1) find the fixed pt by NRaphson method
 #2) jacobian
 
+#Problem:
+# Must ensure that the fixed pts are physical, i.e. check that the orbit is always <sigma
+
+
 from math import *
 import numpy as np
 import numpy.linalg as linalg
@@ -10,25 +14,25 @@ import commands
 import sys
 from random import uniform
 
-sigma=1
-gamma=0.08
-n=4
-w=1
-wg=n*w/2.0
-k=wg**2+(gamma/2.0)**2
-w0=sqrt(k)
-F=0.39
-T=4*pi/w
+sigma=None
+gamma=None
+n=None
+w=None
+wg=None
+k=None
+w0=None
+F=None
+T=None
 
-Sign=1
+M2T=None
+Sign=None
 
-Amp=F/sqrt((w0**2-w**2)**2+(w*gamma)**2)
+Amp=None
 
 def M(t):
 	return exp(-gamma*t/2)/wg*np.matrix([[wg*cos(wg*t)+gamma*sin(wg*t)/2, sin(wg*t)],\
 						[-k*sin(wg*t), wg*cos(wg*t)-gamma*sin(wg*t)/2]])
 
-M2T=M(T)
 
 def G(y):
 	x=float(y[0][0])
@@ -41,7 +45,7 @@ def Jackmap(y):
 	x=float(y[0])
 	if abs(x-sigma)<Amp:
 		return M2T*np.matrix([[1,0],\
-			[2*w*(x-sigma)/sqrt(Amp**2-(sigma-x)**2),-1]])
+			[2*w*Sign*(sigma-x)/sqrt(Amp**2-(sigma-x)**2),-1]])
 	else:
 		return None
 			
@@ -82,11 +86,12 @@ def fp_exact():
 	c=float(M2T[1,0])
 	d=float(M2T[1,1])
 	
+	print "b: ",b	
 	alpha=(a-d+a*d-b*c-1)/(2*b*w)
 
 	discrim=sigma**2-(alpha**2+1)*(sigma**2-Amp**2)
 	if discrim>0:
-		fx=(sigma+sqrt(discrim))/(alpha**2+1)
+		fx=(sigma+sqrt(discrim))/(alpha**2+1)	#Why not -ve sign?
 		fv=(d-a*d+b*c)*fx/b
 		
 		Sign=(fx*(1-a)+b*fv)/(2*b*w*sqrt(Amp**2-(sigma-fx)**2))
@@ -116,42 +121,79 @@ def plot_fp_eigvals(fmin,fmax,npts):
 		abseigs=[abs(i) for i in eigs]
 		abseigs.sort()
 #		print F,float(y[0]),float(y[1]),abs(eigs[0]),abs(eigs[1]), 0
-		print F,float(yexact[0]),float(yexact[1]),abseigs[0],abseigs[1]
+		print F,float(yexact[0]),float(yexact[1]),abseigs[0],abseigs[1],Sign
 		
 		F+=df	
 			
 
 
-sigma=1
-gamma=0.08
-n=float(sys.argv[1])
-w=1
-wg=n*w/2.0
-k=wg**2+(gamma/2.0)**2
-w0=sqrt(k)
-T=2*pi/w
 
+def probe_stability_vs_F(nval,pval):	
+	global sigma,gamma,n,w,wg,k,w0,T,M2T,Sign
+	sigma=1
+	gamma=0.08
+	n=nval
+	w=1
+	wg=n*w/2.0
+	k=wg**2+(gamma/2.0)**2
+	w0=sqrt(k)
+	T=pval*pi/w
+	
+	
+	M2T=M(T)
+	Sign=1
+	
+	Fgraz=sigma*sqrt((w0**2-w**2)**2+(w*gamma)**2)
+	print "#Fgraz: ",Fgraz
+	plot_fp_eigvals(Fgraz*1.001,Fgraz*1.9,100)
 
-M2T=M(T)
-Sign=1
+def probe_stability_vs_n(nmin,nmax):
+	global sigma,gamma,n,w,wg,k,w0,T,M2T,Sign,Amp,F
+	sigma=1
+	gamma=0.08
+	n=nmin
+	w=1
+	n=nmin
+	
+	pval=4
 
-Fgraz=sigma*sqrt((w0**2-w**2)**2+(w*gamma)**2)
-print "#Fgraz: ",Fgraz
-plot_fp_eigvals(Fgraz*1.01,Fgraz*1.9,100)
+	while n<nmax:
+		wg=n*w/2.0
+		k=wg**2+(gamma/2.0)**2
+		w0=sqrt(k)
+		T=pval*pi/w
+		
+		M2T=M(T)
+		Sign=1
 
+		a=float(M2T[0,0])
+		b=float(M2T[0,1])
+		c=float(M2T[1,0])
+		d=float(M2T[1,1])
+	
+		alpha=(a-d+a*d-b*c-1)/(2*b*w)
+		fx=2*sigma/(alpha**2+1)
+		fv=(d-a*d+b*c)*fx/b
+			
+		Sign=1 if (fx*(1-a)+b*fv)*(2*b*w*sqrt(sigma**2-(sigma-fx)**2))>0 else -1
+	
+		Ja=Jackmap([fx,fv])
+		
+		if Ja ==None:
+			n+=0.1
+			continue
 
-#	print fp_exact()
-#	for i in range(100):
-#		try:
-#			y=nraphsonsolve(uniform(sigma-Amp,sigma),uniform(0,3),0.001,5000)
-#			print "y"
-#			print y
-#			print "eigenvals"
-#			print Jackmap(y)
-#		except:
-#			pass	
-#	for x in np.arange(-2,0,0.01):
-#		try:
-#			print x, float(G(np.matrix([x,0.02]).transpose())[0][0])
-#		except:
-#			pass	
+		eigs=linalg.eigvals(Ja)
+		abseigs=[abs(i) for i in eigs]
+		abseigs.sort()
+#		print F,float(y[0]),float(y[1]),abs(eigs[0]),abs(eigs[1]), 0
+		print n,fx,fv,abseigs[0],abseigs[1],Sign
+
+			
+		n+=0.1
+	
+
+if __name__=='__main__':
+#	probe_stability_vs_F(float(sys.argv[1]),int(sys.argv[2]))
+	probe_stability_vs_n(float(sys.argv[1]),int(sys.argv[2]))
+
